ef85a5153c94ad73bd77a79f464d4e75fcd742b7
svenvdz <svenzee@hotmail.nl>
2024-08-07 09:15:20 +0200

    Add checkboxes for Android iOS and web in the onboarding

--- src/pages/onboarding/CreateAppPage.vue
+++ src/pages/onboarding/CreateAppPage.vue
@@ -24,35 +24,35 @@
               <input v-model="name" type="text" name="project-name" id="project-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
-<!--          <div>-->
-<!--            <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Available on</label>-->
-<!--            <div class="flex items-center">-->
-<!--              <div class="flex items-center h-5">-->
-<!--                <input v-model="web" id="web" aria-describedby="web" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-600">-->
-<!--              </div>-->
-<!--              <div class="ml-3 text">-->
-<!--                <label for="web" class="text-gray-600">Web</label>-->
-<!--              </div>-->
-<!--            </div>-->
+          <div>
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Available on</label>
+            <div class="flex items-center">
+              <div class="flex items-center h-5">
+                <input v-model="web" id="web" aria-describedby="web" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-600">
+              </div>
+              <div class="ml-3 text">
+                <label for="web" class="text-gray-600">Web</label>
+              </div>
+            </div>
 
-<!--            <div class="flex items-center">-->
-<!--              <div class="flex items-center h-5">-->
-<!--                <input v-model="android" id="android" aria-describedby="android" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-600">-->
-<!--              </div>-->
-<!--              <div class="ml-3 text">-->
-<!--                <label for="android" class="text-gray-600">Android</label>-->
-<!--              </div>-->
-<!--            </div>-->
+            <div class="flex items-center">
+              <div class="flex items-center h-5">
+                <input v-model="android" id="android" aria-describedby="android" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-600">
+              </div>
+              <div class="ml-3 text">
+                <label for="android" class="text-gray-600">Android</label>
+              </div>
+            </div>
 
-<!--            <div class="flex items-center">-->
-<!--              <div class="flex items-center h-5">-->
-<!--                <input v-model="ios" id="ios" aria-describedby="ios" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-600">-->
-<!--              </div>-->
-<!--              <div class="ml-3 text">-->
-<!--                <label for="ios" class="text-gray-600">iOS</label>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
+            <div class="flex items-center">
+              <div class="flex items-center h-5">
+                <input v-model="ios" id="ios" aria-describedby="ios" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-600">
+              </div>
+              <div class="ml-3 text">
+                <label for="ios" class="text-gray-600">iOS</label>
+              </div>
+            </div>
+          </div>
           <button @click="createAppClicked" type="submit" class="rounded-md w-full bg-primary-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500">
             Create App
           </button>
@@ -68,7 +68,7 @@ export default {
   data() {
     return {
       name: null,
-      web: true,
+      web: false,
       android: false,
       ios: false
     }
